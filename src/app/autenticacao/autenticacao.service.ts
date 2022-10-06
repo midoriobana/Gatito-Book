5171f0336e7baa4f703f5195f4a609c152473193
midoriobana <ingridmidori@live.com>
2022-10-06 16:49:05 -0300

    Criação da tela de login e serviços de rotas

---
+++ src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AutenticacaoService {
+
+  endpoint = environment.API_URL
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  autenticar(usuario: string, senha: string): Observable<any> {
+    return this.http.post<any>(`${this.endpoint}/user/login`, {
+      userName: usuario,
+      password: senha
+    })
+  }
+}
